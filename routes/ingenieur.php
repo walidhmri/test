619aee36e71840c3e26e641f6e1bd312153c5405
Oualid Hamri <whmri7@gmail.com>
2025-04-21 10:21:49 +0100

    fix bugs

--- routes/ingenieur.php
+++ routes/ingenieur.php
@@ -1,6 +1,10 @@
 <?php
+use App\Http\Controllers\Ingenieur\CommentController;
 use App\Http\Controllers\Ingenieur\SolutionController;
 use App\Http\Controllers\Ingenieur\TicketController;
+use App\Models\Comment;
+use App\Models\ticket;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Ingenieur\ProfileController;
 
@@ -12,12 +16,23 @@ use App\Http\Controllers\Ingenieur\ProfileController;
 
 Route::middleware(['auth', 'ingenieurMiddleware'])->group(function () {
     Route::get('/ingenieur/dashboard', function () {
+        $tickets = ticket::where('assign', auth()->user()->id)->orWhere('department_id', Auth::user()->department_id)->get();
+        $comments = Comment::whereIn('ticket_id', $tickets->pluck('id'))->get();
+        return view('ingenieur.dashboard',compact('comments', 'tickets'));
+    })->name('ingenieur.dashboard');
+
+    Route::get('/ingenieur', function () {
         return view('ingenieur.dashboard');
     })->name('ingenieur.dashboard');
     Route::get('ingenieur/profile', [ProfileController::class, 'index'])->name('ingenieur.profile.show');
     Route::get('ingenieur/settings', [ProfileController::class, 'update'])->name('ingenieur.profile.update');
     Route::get('ingenieur/tickets',[TicketController::class, 'index'])->name('ingenieur.ticket.list');
     Route::get('ingenieur/tickets/{id}/show',[TicketController::class, 'show'])->name('ingenieur.ticket.show');
+    Route::get('ingenieur/comments',[CommentController::class, 'index'])->name('ingenieur.comments.list');
+
+
+
+
     Route::patch('ingenieur/tickets/{id}/update',[TicketController::class, 'update'])->name('ingenieur.ticket.update');
     Route::get('ingenieur/tickets/{id}/solve',[SolutionController::class, 'index'])->name('ingenieur.ticket.solve');
     Route::post('ingenieur/tickets/solution/add',[SolutionController::class, 'store'])->name('ingenieur.ticket.solution.store');
