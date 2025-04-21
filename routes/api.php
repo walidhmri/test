9f27333bb8b38332c54799f2cd62f99be9c5325f
Oualid Hamri <whmri7@gmail.com>
2025-04-21 11:09:31 +0100

    remove login and register routes from API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+
 
